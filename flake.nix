e95330966588262370b3312a5e7b74f72b62ea03
Guilherme Grochau Azzi <gazzi@babbel.com>
2022-12-14 22:38:06 +0100

    Add scripts for opening GitHub projects

--- flake.nix
+++ flake.nix
@@ -8,13 +8,20 @@
   };
 
   outputs = { self, flake-utils, nixpkgs, sub }:
-    flake-utils.lib.eachDefaultSystem (system: {
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = sub.lib.${system}.mkSubDerivation {
+          pname = "dev-cli-utils";
+          cmd = "dev";
+          version = "0.1.0";
+          src = ./.;
 
-      packages.default = sub.lib.${system}.mkSubDerivation {
-        pname = "dev-cli-utils";
-        cmd = "dev";
-        version = "0.1.0";
-        src = ./.;
-      };
-    });
+          propagatedBuildInputs = with pkgs; [
+            gh
+            tmux
+          ];
+        };
+      });
 }
